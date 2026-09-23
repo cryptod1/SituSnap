2d5b4838b9e74918acd413bc9f4e8762e06c4af6
cryptod1 <94923026+cryptod1@users.noreply.github.com>
2026-09-23 10:31:38 +0100

    make_android.py

--- .github/scripts/make_android.py
+++ .github/scripts/make_android.py
@@ -26,8 +26,8 @@ android {
         applicationId 'com.situsnap.app'
         minSdk 26
         targetSdk 35
-        versionCode 1
-        versionName '0.1-CLOUD7'
+        versionCode 2
+        versionName '0.2-CLOUD8'
     }
 }
 """)
@@ -36,16 +36,50 @@ put("app/src/main/res/values/styles.xml", """<resources>
 <style name="AppTheme" parent="android:style/Theme.Material.Light.NoActionBar">
 <item name="android:statusBarColor">#07192b</item>
 <item name="android:navigationBarColor">#07192b</item>
+<item name="android:windowLightStatusBar">false</item>
+<item name="android:windowLightNavigationBar">false</item>
+<item name="android:windowOptOutEdgeToEdgeEnforcement">true</item>
 </style>
 </resources>
 """)
+
+put("app/src/main/res/drawable/situsnap_icon.xml", """<vector xmlns:android="http://schemas.android.com/apk/res/android"
+    android:width="108dp" android:height="108dp"
+    android:viewportWidth="108" android:viewportHeight="108">
+    <path android:fillColor="#07192B" android:pathData="M0,0h108v108h-108z"/>
+    <path android:fillColor="#FFFFFF" android:pathData="M20,16h22v6h-16v16h-6zM66,16h22v22h-6v-16h-16zM20,70h6v16h16v6h-22zM82,70h6v22h-22v-6h16z"/>
+    <path android:fillColor="#2FA866" android:pathData="M54,25c-13,0 -23,10 -23,23c0,18 23,38 23,38s23,-20 23,-38c0,-13 -10,-23 -23,-23z"/>
+    <path android:fillColor="#FFFFFF" android:pathData="M54,34a14,14 0,1 0,0 28a14,14 0,1 0,0 -28z"/>
+    <path android:fillColor="#2FA866" android:pathData="M41,55l8,-9l6,6l5,-5l8,8v4h-27z"/>
+    <path android:strokeColor="#FFFFFF" android:strokeWidth="3" android:strokeLineCap="round"
+        android:fillColor="@android:color/transparent" android:pathData="M53,60c-3,5 -5,9 -4,15c1,5 5,8 7,11"/>
+</vector>
+""")
+
+put("app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml", """<adaptive-icon xmlns:android="http://schemas.android.com/apk/res/android">
+    <background android:drawable="@color/icon_background"/>
+    <foreground android:drawable="@drawable/situsnap_icon"/>
+</adaptive-icon>
+""")
+
+put("app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml", """<adaptive-icon xmlns:android="http://schemas.android.com/apk/res/android">
+    <background android:drawable="@color/icon_background"/>
+    <foreground android:drawable="@drawable/situsnap_icon"/>
+</adaptive-icon>
+""")
+
+put("app/src/main/res/values/colors.xml", """<resources>
+    <color name="icon_background">#07192B</color>
+</resources>
+""")
+
 put("app/src/main/AndroidManifest.xml", """<manifest xmlns:android="http://schemas.android.com/apk/res/android">
 <uses-permission android:name="android.permission.INTERNET"/>
 <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>
 <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
 <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/>
 <uses-permission android:name="android.permission.CAMERA"/>
-<application android:theme="@style/AppTheme" android:label="SituSnap" android:usesCleartextTraffic="false">
+<application android:theme="@style/AppTheme" android:label="SituSnap" android:icon="@mipmap/ic_launcher" android:roundIcon="@mipmap/ic_launcher_round" android:usesCleartextTraffic="false">
 <activity android:name=".MainActivity" android:exported="true">
 <intent-filter>
 <action android:name="android.intent.action.MAIN"/>
@@ -91,6 +125,9 @@ public class MainActivity extends Activity {
         s.setDomStorageEnabled(true);
         s.setDatabaseEnabled(true);
         s.setGeolocationEnabled(true);
+        s.setTextZoom(100);
+        s.setUseWideViewPort(true);
+        s.setLoadWithOverviewMode(false);
 
         webView.setWebViewClient(new WebViewClient() {
             @Override
