a7d295d4c9c94944b953d7a53c8a9d5e159860dc
cryptod1 <94923026+cryptod1@users.noreply.github.com>
2026-09-25 17:49:39 +0100

    applicationId 'com.situsnap.app.release'

--- .github/scripts/make_android.py
+++ .github/scripts/make_android.py
@@ -23,7 +23,7 @@ android {
     namespace 'com.situsnap.app'
     compileSdk 35
     defaultConfig {
-        applicationId 'com.situsnap.app'
+        applicationId 'com.situsnap.app.release'
         minSdk 26
         targetSdk 35
         versionCode 1
