15188696e9bc1cbb8d537ae2ad8deca05d9f7432
cryptod1 <94923026+cryptod1@users.noreply.github.com>
2026-09-26 15:12:34 +0100

    Update make_android.py

--- .github/scripts/make_android.py
+++ .github/scripts/make_android.py
@@ -26,8 +26,8 @@ android {
         applicationId 'com.situsnap.app'
         minSdk 26
         targetSdk 35
-        versionCode 1
-        versionName '0.1-CLOUD7'
+        versionCode 6
+        versionName '1.0.6'
     }
 
     signingConfigs {
